94b0b6b7da4c1380d0bd105d362559a8d3dbbca7
YahyaAbha <ffpubglite9@gmail.com>
2024-10-21 23:43:02 +0500

    master

---
+++ 21-25/script.js
@@ -0,0 +1,71 @@
+// let firstName = prompt("Enter your first name");
+// let lastName = prompt("Enter your last name");
+// let fullName = firstName + lastName;
+// let greet = "Assalamuaalaikum";
+// if(!firstName || !lastName){
+//     console.log("Invalid input");
+// } else {
+//     console.log(greet,fullName);
+// }
+
+
+// let user = " Samsung Galaxy S6 Edge Plus";
+// let i  = user.length;
+// console.log("My favorite phone is:",user,"Length of string:",i);
+
+
+
+// let index = "Pakistani";
+// let find = index.indexOf("n");
+// console.log(find);
+
+
+// let word = "Hello World";
+// let find = word.lastIndexOf("l");
+// console.log("String:",word);
+// console.log("Last index of 'l':",find);
+
+
+// let word = "Pakistani";
+// let index = 3;
+// let newFind = word[index];
+// let find = word.indexOf(word[index]);
+// console.log("String:",word);
+// console.log("Character at index 3:",newFind);
+
+
+// let user1 = prompt("Enter first name");
+// let user2 = prompt("Enter last name");
+// let user3 = user1 +" "+ user2;
+
+// if(!user1 || !user2){
+//     console.log("Invalid input");
+// } else {
+//     console.log("Hello",user3);
+// }
+
+// var text = "World War II"
+// var firstChar = text.indexOf("World War II");
+//  if (firstChar !== -1) {
+//      text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+//      console.log(firstChar);
+// }
+
+// let text = "Hyderabad";
+// var newText = text.replace("Hyderabad", "Islamabad");
+// console.log(newText);
+
+    var change = "“Ali and Sami are best friends. They play cricket and football together.”";
+    var newStr = change.replace(/and/g, "&");
+    console.log(newStr);
+
+
+ 
+    
+
+
+
+
+
+
+
