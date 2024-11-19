1deee2241173e21ba0c4c7df4d86a4cc03da8923
YahyaAbha <ffpubglite9@gmail.com>
2024-11-19 19:29:58 +0500

    master

--- 35-38/script.js
+++ 35-38/script.js
@@ -43,15 +43,7 @@
 
 
 
-// var user = +prompt("enter the number");
-// var random = Math.random()*user;
-// var round = Math.round(random);
-// var floor = Math.floor(random);
-// var ceil = Math.ceil(random);
-// console.log("number:",random);
-// console.log("round off value:",round);
-// console.log("floor value:",floor);
-// console.log("ceil value:",ceil);
+
 
 
 
