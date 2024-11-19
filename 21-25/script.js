361cbbcb66c781dbc65e51a2dd889ebaadda8ef3
YahyaAbha <ffpubglite9@gmail.com>
2024-11-19 17:15:16 +0500

    master

--- 21-25/script.js
+++ 21-25/script.js
@@ -90,28 +90,92 @@
 
 
 
-function validateUsername() {
-    // Get the input value from the user
-    var username = document.getElementById("username").value;
+// function validateUsername() {
+//     // Get the input value from the user
+//     var username = document.getElementById("username").value;
 
-    // Define a regex pattern that matches any of the special characters [@, !, ., ,]
-    var invalidChars = /[@!.,]/;
+//     // Define a regex pattern that matches any of the special characters [@, !, ., ,]
+// var invalidChars = /[@!.,]/;
 
-    // Check if the username contains any of the invalid characters
-    if (invalidChars.test(username)) {
-        // If true, show an alert to prompt user for valid username
-        alert("Please enter a valid username. Special characters @, !, ., , are not allowed.");
-    } else {
-        // If no invalid characters, show success message or proceed further
-        alert("Username is valid!");
-    }
-}
- 
-    
+//     // Check if the username contains any of the invalid characters
+//     if (invalidChars.test(username)) {
+//         // If true, show an alert to prompt user for valid username
+//         alert("Please enter a valid username. Special characters @, !, ., , are not allowed.");
+//     } else {
+//         // If no invalid characters, show success message or proceed further
+//         alert("Username is valid!");
+//     }
+// }
+
+// let user = prompt("Enter the name"); 
+// let char = user.fromCharAt(33,44,46,64);
+// for(let i = 0 ; i < user.length; i++){
+//     if(char  === user[i]){
+//         console.log("Valid name");
+//     }
+// }
 
 
+// var validate = (e,email)=>{
+//     e.preventDefault();
+//     var email = document.getElementById('email').value;
+//     var fire = email.charCodeAt(64);
+//     var fire2 = 'gmail.com';
+
+//     if (email == "" || email == fire + fire2){
+//         console.log("Invalid email 1");
+//     }
+//     else{
+//         console.log("valid email 3");
+//     }
+
+// }
 
 
+// var a = prompt("Enter the name").toLowerCase();
+// var c = false;
+// var b = ["cake", "apple pie", "cookie", "chips", "patties"];
+// for (let i = 0; i < b.length; i++) {
+//     if (a === b[i]) {
+//         c = true;
+//         break;
+//     }
+// }
+
+// if(c) {
+//     console.log(`${a} is available in our bakery`);
+// } else {
+//     console.log("the name is not in list");
+// }
 
 
+// var user = prompt('enter the password');
+// if(user.match(/[0-9]/) && user.match(/[a-zA-Z]/)) {
+//     if(user.charAt(0).match(/[a-zA-Z]/)) {
+//         if(user.length < 6){
+//             console.log('password is too short');
+//         } else {
+//             console.log('password is strong');
+//         }
+//     } else {
+//         console.log("Password should start with a alphabet")
+//     }
+// } else {
+//     console.log("Password should contain Numbers and alphabets.")
+// }
 
+// var arr = [];
+// var university = "University of Karachi";
+// for(let i = 0; i < university.length; i++){
+//     arr.push(university[i])
+//     console.log(arr[i]);
+// }
+
+
+// var pak = 'Pakistan'
+// var last = pak.slice(-1);
+// console.log(last);
+
+// var str = "The quick brown fox jumps over the lazy dog.";
+// var find = str.toLowerCase().split(" ").filter(word => word === "the").length;
+// console.log(find);
