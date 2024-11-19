03d7e2dda4c1f05a91c550f838a7ac0dae2393d8
YahyaAbha <ffpubglite9@gmail.com>
2024-11-19 18:24:06 +0500

    master

---
+++ 35-38/script.js
@@ -0,0 +1,45 @@
+// function date(){
+//     var curruntDate = new Date();
+//     console.log(curruntDate);
+// }
+// date();
+
+// var greet = ()=>{
+//     var first = prompt("enter first name");
+//     var last = prompt("enter last name");
+//     console.log(`Hello ${first} ${last}`);
+// }
+// greet();
+
+
+// var add = (a,b)=>{
+//     var a;
+//     var b;
+//     var c = a+b;
+//     console.log(`The sum of ${a} and ${b} is ${c}`);
+    
+// }
+// add(3,4);
+
+// var num1 = +prompt("enter first number");
+// var num2 = +prompt("enter second number");
+// var ope = prompt("enter operator");
+
+// var cal = (num1,num2,ope)=>{
+//     if(ope == '+'){
+//         var result = num1+num2;
+//     } else if(ope == '-'){
+//         var result = num1-num2;
+//     } else if(ope == '*'){
+//         var result = num1*num2;
+//     } else if(ope == '/'){
+//         var result = num1/num2;
+//     } else {
+//         var result = "Invalid operator";
+//     }
+//     console.log(`The result of ${num1} ${ope} ${num2} is ${result}`);
+// }
+// cal(num1,num2,ope);
+
+
+
