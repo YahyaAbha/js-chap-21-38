c475bbc94ad58d7778d249917cd31c7926f6010c
YahyaAbha <ffpubglite9@gmail.com>
2024-11-19 20:41:17 +0500

    master

--- 31-34/script.js
+++ 31-34/script.js
@@ -1,2 +1,46 @@
-var currnt = new Date();
-console.log(currnt);
+// var currnt = new Date();
+// console.log(currnt);
+
+
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var month = new Date().getMonth();
+// console.log("current month is:",months[month]);
+
+
+// var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var day = new Date().getDay();
+// alert("current day is: " + days[day]);
+
+
+// var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var day = new Date().getDay();
+// if(days[day] == "Sunday" || days[day] == "Saturday"){
+//     console.log("“It’s Fun day”");
+// }
+// else{
+//     console.log("“It’s not Fun day”");
+// }
+
+// var date = new Date().getDate();
+// if(date < 16){
+//     console.log("First fifteen days of the month");
+// }
+// else{
+//     console.log("Last days of the month");
+// }
+
+
+
+// var currentTime = new Date();
+// console.log("current time is:",currentTime);
+// var milli = currentTime.getTime();
+// console.log("Elapsed milliseconds since January 1,1970: ",milli);
+// var mint = milli /(1000 *60);
+// console.log(mint);
+
+
+var time = new Date();
+console.log(time);
+
+
+
